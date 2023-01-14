9148115ac625051af6e5d04c259f1ff29b3cf5c5
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-01-14 14:52:45 -0500

    added questions div in HTML and created function to start the quiz, hide the start screen and show the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Select HTML elements needed to make the JS code work
 var timerEl = document.querySelector(".timer");
 var startQuizEl = document.querySelector("#start-quiz");
-
+var startScreenEl = document.querySelector("#start-screen");
+var questionsEl = document.querySelector("#questions-screen");
 
 // Function to create a countdown for 100-0 seconds
 function countdown() {
@@ -16,5 +17,11 @@ function countdown() {
     }, 1000);
 }
 
+// Function to hide the start screen and move on to the questions
+function startQuiz() {
+    startScreenEl.setAttribute("hidden", "hidden");
+    questionsEl.removeAttribute("hidden");
+}
+
 // When the "start quiz" button is pressed, the timer will start counting down from 100
-startQuizEl.addEventListener("click", countdown);
+startQuizEl.addEventListener("click", startQuiz);
